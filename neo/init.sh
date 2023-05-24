b4d757c42667aef18811a1b66003bc084033c023
QJAutumn <780588304@qq.com>
2023-05-24 19:54:59 +0800

    [change]修改

--- neo/init.sh
+++ neo/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "  - \"http://$YAO_DOMAIN:5099\"" >> ./neo/neo.yml  
+echo "  - \"$YAO_DOMAIN\"" >> ./neo/neo.yml  
 yao migrate && yao run scripts.setup.Data && /usr/local/bin/yao start
